774892c89275e1da59c7fafdd6112c4f2f74c8ef
workstation <workstation@PC.local>
2026-04-01 13:16:41 +0000

    No message

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -85,7 +85,7 @@ const Auth = () => {
     options: {
       // This ensures that after Google login, it brings the user 
       // back to your local development site
-      redirectTo: 'https://elearning-platform-frontend-one.vercel.app/', 
+      redirectTo: window.location.origin, 
     },
   });
   
